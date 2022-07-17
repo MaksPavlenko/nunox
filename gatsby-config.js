954e431a29eda68184e735d06b7ad56d19175308
Maksim Pavlenko <user@MacBook-Pro-Maksim.local>
2022-07-17 11:25:23 +0300

    sm v3

--- gatsby-config.js
+++ gatsby-config.js
@@ -97,61 +97,7 @@ module.exports = {
         ],
       },
     },
-    `gatsby-plugin-advanced-sitemap`// {
-    //   resolve: 'gatsby-plugin-sitemap',
-    //   options: {
-    //     excludes: ['/**/404', '/**/404.html'],
-    //     query: `
-    //       {
-    //         site {
-    //           siteMetadata {
-    //             siteUrl
-    //           }
-    //         }
-    //         allSitePage(filter: {context: {i18n: {routed: {eq: false}}}}) {
-    //           edges {
-    //             node {
-    //               context {
-    //                 i18n {
-    //                   defaultLanguage
-    //                   languages
-    //                   originalPath
-    //                 }
-    //               }
-    //               path
-    //             }
-    //           }
-    //         }
-    //       }
-    //     `,
-    //     resolveSiteUrl: () => siteUrl,
-    //     serialize: ({ site, allSitePage }) => {
-    //       return allSitePage.edges.map((edge) => {
-    //         const {
-    //           languages,
-    //           originalPath,
-    //           defaultLanguage,
-    //         } = edge.node.context.i18n;
-    //         // const { siteUrl } = site.siteMetadata;
-    //         const url = siteUrl + originalPath;
-    //         const links = [
-    //           { lang: defaultLanguage, url },
-    //           { lang: 'x-default', url },
-    //         ];
-    //         languages.forEach((lang) => {
-    //           if (lang === defaultLanguage) return;
-    //           links.push({ lang, url: `${siteUrl}/${lang}${originalPath}` });
-    //         });
-    //         return {
-    //           url,
-    //           changefreq: 'daily',
-    //           priority: originalPath === '/' ? 1.0 : 0.7,
-    //           links,
-    //         };
-    //       });
-    //     },
-    //   },
-    // },
+    `gatsby-plugin-advanced-sitemap`,
     `gatsby-plugin-sass`,
 
     {
