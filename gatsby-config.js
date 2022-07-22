3ab2af8c98f97dfd3467d0377dc216884889889e
Maksim Pavlenko <user@MacBook-Pro-Maksim.local>
2022-07-22 11:32:09 +0300

    analitics

--- gatsby-config.js
+++ gatsby-config.js
@@ -99,6 +99,38 @@ module.exports = {
       },
     },
     `gatsby-plugin-advanced-sitemap`,
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'UA-138246887-1', // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          // optimize_id: 'OPT_CONTAINER_ID',
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Defaults to https://www.googletagmanager.com
+          // origin: "YOUR_SELF_HOSTED_ORIGIN",
+        },
+      },
+    },
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     trackingId: "UA-138246887-1",
+    //     head: true,
+    //   },
+    // },
     `gatsby-plugin-sass`,
 
     {
